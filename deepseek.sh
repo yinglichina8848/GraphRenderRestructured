7b7b2738da81297df43a0a03d646a1ea7e19bc85
Li Ying <1820393151@qq.com>
2025-06-19 10:50:48 +0800

    加入 脚本：deepseek.sh, xinference.sh

---
+++ deepseek.sh
@@ -0,0 +1,7 @@
+# 设置环境变量（根据实际端口调整）
+#export OPENAI_API_BASE=http://192.168.0.252:9998/v1
+#export OPENAI_API_KEY=sk-no-key-required
+#export OPENAI_MODEL=openai/DeepSeek-R1-0528-Qwen3-8B
+export DEEPSEEK_API_KEY=sk-953706e5e2ca40faa826f269d29dc8a5
+
+aider --model deepseek/deepseek-chat
